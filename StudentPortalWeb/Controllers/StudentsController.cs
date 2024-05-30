3058a80c09d09ffc896ccad539342e3bc275affc
Salman Aslam <Salman.Aslam@Stewart.com>
2024-05-30 23:05:52 +0500

    Add project files.

---
+++ StudentPortalWeb/Controllers/StudentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortalWeb.Models;
+using StudentPortalWeb.Data;
+
+namespace StudentPortalWeb.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;            
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name;
+                
+            }
+            await dbContext.Students.AddAsync();
+            return View();
+        }
+    }
+}
