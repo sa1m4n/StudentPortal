5b9f2ea7ba6500fa64af0e2fbb5f8ea32f5bdf8e
Salman Aslam <Salman.Aslam@Stewart.com>
2024-05-31 17:59:35 +0500

    Delete Function Added

--- StudentPortalWeb/Controllers/StudentsController.cs
+++ StudentPortalWeb/Controllers/StudentsController.cs
@@ -72,5 +72,21 @@ namespace StudentPortalWeb.Controllers
 
             return RedirectToAction("List", "Students");
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == viewModel.Id);
+
+            if(student is not null)
+            {
+                dbContext.Students.Remove(viewModel);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Students");
+        }
     }
 }
